767800c76ff10f6bdadd9950012ce0e6627cf9ed
hericsongogola <hericsongogola@gmail.com>
2024-11-28 22:00:47 -0300

    inicio da logica

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+const inputNome = document.getElementById("nome");
+const inputSobrenome = document.getElementById("sobrenome");
+const inputRg = document.getElementById("rg");
+const inputCpf = document.getElementById("cpf");
+const inputRadioMasculino = document.getElementById("radioMasculino");
+const inputRadioFeminino = document.getElementById("radioFeminino");
+const inputCargo = document.getElementById("cargo");
+const inputSenioridade = document.getElementById("senioridade");
+
+const buttonLimpar = document.getElementById("limpar");
+const buttonSalvar = document.getElementById("salvar");
+
+buttonSalvar.addEventListener("click", () => {
+  console.log(
+    inputNome.value,
+    inputSobrenome.value,
+    inputRg.value,
+    inputCpf.value,
+    inputRadioMasculino.checked,
+    inputRadioFeminino.checked,
+    inputCargo.value,
+    inputSenioridade.value
+  );
+});
